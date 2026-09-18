fa146ba62f8973453e0e48b623fd4d87cb1d4e6c
Julia Vargas <juliavargaspastors@gmail.com>
2026-09-18 10:26:08 +0200

    a

---
+++ U1/Programas_06/programa01.py
@@ -0,0 +1,8 @@
+entero = 5
+decimal = 5.5
+booleano = False
+cadena = "holi"
+print(entero, type(entero))
+print(decimal, type(decimal))
+print(booleano, type(booleano))
+print(cadena, type(cadena))
---
+++ U1/Programas_06/programa02.py
@@ -0,0 +1,3 @@
+num = 6
+print(type(6))
+print(type(num))
